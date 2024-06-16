1dba36fb235ee1fbaf8d6da816f7392a43a457fe
wwan13 <wwan13@naver.com>
2024-06-16 19:18:18 +0900

    feat : Init kotlin-starter project

---
+++ kotlin/kotlin-starter/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+	id("org.springframework.boot") version "2.7.13"
+	id("io.spring.dependency-management") version "1.1.5"
+	kotlin("jvm") version "1.9.24"
+	kotlin("plugin.spring") version "1.9.24"
+}
+
+group = "io.wwan13"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ kotlin/kotlin-starter/src/test/kotlin/io/wwan13/kotlinstarter/KotlinStarterApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.wwan13.kotlin_starter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinStarterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

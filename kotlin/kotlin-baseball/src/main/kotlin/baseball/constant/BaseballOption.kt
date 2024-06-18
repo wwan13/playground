b5958bf477b07c34df9146f0ae1ffb1ab02888df
wwan13 <wwan13@naver.com>
2024-06-18 13:58:53 +0900

    feat : 상수 선언

--- kotlin/kotlin-baseball/src/main/kotlin/baseball/constant/BaseballOption.kt
+++ kotlin/kotlin-baseball/src/main/kotlin/baseball/constant/BaseballOption.kt
@@ -3,3 +3,8 @@ package baseball.constant
 const val BASEBALL_NUMBER_MIN_VALUE = 1
 const val BASEBALL_NUMBER_MAX_VALUE = 9
 const val BASEBALL_NUMBER_SIZE = 3
+
+const val ANSWER_STRIKE_VALUE = 3
+
+const val GAME_STATUS_RESTART = 1
+const val GAME_STATUS_END = 2
